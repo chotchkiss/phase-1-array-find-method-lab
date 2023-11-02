9558246aea35825a7b9c313bee9ff283472f949f
Cheyenne Hotchkiss <cheycheymh@gmail.com>
2023-11-02 05:08:46 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+  function superbowlWin(array){
+    const result = array.find((el) => el.result === 'W')
+    if(result) return  result.year
+    else return result
+}
